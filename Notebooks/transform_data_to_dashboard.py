8f4642ed0862a1e23f2692a2aba8fe6456bfeec8
Aoch24 <Aoch24>
2024-11-01 06:35:47 +0000

    visual

--- Notebooks/transform_data_to_dashboard.py
+++ Notebooks/transform_data_to_dashboard.py
@@ -22,10 +22,6 @@ df_vendas.display()
 
 # COMMAND ----------
 
-df_vendas.display()
-
-# COMMAND ----------
-
 df_tendencia_presente = spark.sql("""
 SELECT        
 fact_tendencia.id_loja, 
@@ -51,36 +47,135 @@ df_tendencia_presente.createOrReplaceTempView("tendencia_presente_view")
 
 # COMMAND ----------
 
+df_vendas_realizadas = spark.sql("""
+SELECT
+id_loja,
+id_dia,
+id_producto,
+sum(cant_vendida) as cant_vendida,
+sum(valor_venta) as valor_venta,
+sum(costo_venta) as costo_venta
+FROM
+    fact_venta
+GROUP BY id_loja, id_dia, id_producto
+ORDER BY id_dia, id_loja, id_producto
+    """)
+
+
+# COMMAND ----------
+
+df_vendas_realizadas.createOrReplaceTempView("vendas_realizadas_view")
+
+# COMMAND ----------
+
 df_kpis_vendas = spark.sql("""
 SELECT
-tp.id_loja,
-tp.id_dia,
-tp.id_producto,
-v.cant_vendida,
-v.valor_venta,
-v.costo_venta,
-o.quantidade_vendida_orcado,
-o.valor_venda_orcado,
-o.custo_venda_orcado,
-tp.quantidade_vendida_tend,
-tp.valor_venda_tend,
-tp.custo_venda_tend
-FROM tendencia_presente_view tp
-INNER JOIN fact_venta v ON tp.id_loja = v.id_loja AND tp.id_dia = v.id_dia AND tp.id_producto = v.id_producto
-INNER JOIN fact_orcado o ON tp.id_loja = o.id_loja AND tp.id_dia = o.id_dia AND tp.id_producto = o.id_producto
+    COALESCE(v.id_loja, o.id_loja) AS id_loja,
+    COALESCE(v.id_dia, o.id_dia) AS id_dia,
+    COALESCE(v.id_producto, o.id_producto) AS id_producto,
+    v.cant_vendida,
+    v.valor_venta,
+    v.costo_venta,
+    o.quantidade_vendida_orcado,
+    o.valor_venda_orcado,
+    o.custo_venda_orcado,
+    tp.quantidade_vendida_tend,
+    tp.valor_venda_tend,
+    tp.custo_venda_tend
+FROM
+    vendas_realizadas_view v
+FULL OUTER JOIN
+    fact_orcado o
+ON
+    v.id_loja = o.id_loja AND v.id_dia = o.id_dia AND v.id_producto = o.id_producto
+LEFT JOIN
+    tendencia_presente_view tp
+ON
+    COALESCE(v.id_loja, o.id_loja) = tp.id_loja
+    AND COALESCE(v.id_dia, o.id_dia) = tp.id_dia
+    AND COALESCE(v.id_producto, o.id_producto) = tp.id_producto;
           """)
 
 display(df_kpis_vendas)
 
 # COMMAND ----------
 
-df_kpis_vendas = df_kpis_vendas.withColumn('kpi_quantidade_real_orcado',f.col('cant_vendida') / f.col('quantidade_vendida_orcado'))\
-                                .withColumn('kpi_valor_venda_real_orcado',f.col('valor_venta') / f.col('valor_venda_orcado'))\
-                                .withColumn('kpi_quantidade_tendencia_orcado',f.col('quantidade_vendida_tend') / f.col('quantidade_vendida_orcado'))\
-                                .withColumn('kpi_valor__venda_tendencia_orcado',f.col('valor_venda_tend') / f.col('valor_venda_orcado'))
-
-display(df_kpis_vendas)
+df_kpis_vendas.createOrReplaceTempView("kpis_vendas_view")
 
 # COMMAND ----------
 
-display(df_kpis_vendas)
+df_kpis_vendas_consolidado = spark.sql("""
+SELECT
+t.cod_mes, 
+t.cod_trimestre,
+t.cod_semestre,
+t.cod_ano,
+sum(k.cant_vendida) as total_quantidade_vendida,
+sum(k.valor_venta) as total_valor_venda,
+sum(k.costo_venta) as total_custo_venda,
+sum(k.quantidade_vendida_orcado) as total_quantidade_vendida_orcado,
+sum(k.valor_venda_orcado) as total_valor_venda_orcado,
+sum(k.custo_venda_orcado) as total_custo_venda_orcado,
+sum(k.quantidade_vendida_tend) as total_quantidade_vendida_tend,
+sum(k.valor_venda_tend) as total_valor_venda_tend,
+sum(k.custo_venda_tend) as total_custo_venda_tend
+FROM kpis_vendas_view k
+INNER JOIN dim_tiempo t ON k.id_dia = t.id_dia
+INNER JOIN dim_producto p ON k.id_producto = p.id_producto
+--WHERE k.id_loja = 10 AND p.cod_sector = 'Bebidas'
+GROUP BY t.cod_mes, t.cod_trimestre, t.cod_semestre, t.cod_ano
+          """)
+
+display(df_kpis_vendas_consolidado.orderBy("cod_mes"))
+
+# COMMAND ----------
+
+df_kpis_vendas_consolidado = df_kpis_vendas_consolidado.withColumn('kpi_quantidade_real_orcado',f.col('total_quantidade_vendida') / f.col('total_quantidade_vendida_orcado'))\
+                                .withColumn('kpi_valor_venda_real_orcado',f.col('total_valor_venda') / f.col('total_valor_venda_orcado'))\
+                                .withColumn('kpi_quantidade_tendencia_orcado',f.col('total_quantidade_vendida_tend') / f.col('total_quantidade_vendida_orcado'))\
+                                .withColumn('kpi_valor__venda_tendencia_orcado',f.col('total_valor_venda_tend') / f.col('total_valor_venda_orcado'))
+
+display(df_kpis_vendas_consolidado)
+
+# COMMAND ----------
+
+df_kpis_vendas_consolidado = df_kpis_vendas_consolidado.withColumn(
+    "KPI_Quantidade_Como_Estou",
+    f.when(f.col("kpi_quantidade_real_orcado") <= 0.9, -1)
+     .when((f.col("kpi_quantidade_real_orcado") > 0.9) & (f.col("kpi_quantidade_real_orcado") < 1.1), 0)
+     .when(f.col("kpi_quantidade_real_orcado") > 1.1, 1)
+)
+
+df_kpis_vendas_consolidado = df_kpis_vendas_consolidado.withColumn(
+    "KPI_Quantidade_Como_Estarei",
+    f.when(f.col("kpi_quantidade_tendencia_orcado") <= 0.9, -1)
+     .when((f.col("kpi_quantidade_tendencia_orcado") > 0.9) & (f.col("kpi_quantidade_tendencia_orcado") < 1.1), 0)
+     .when(f.col("kpi_quantidade_tendencia_orcado") > 1.1, 1)
+)
+
+# COMMAND ----------
+
+df_kpis_vendas_consolidado = df_kpis_vendas_consolidado.withColumn(
+    "KPI_Valor_da_Venda_Como_Estou",
+    f.when(f.col("kpi_valor_venda_real_orcado") <= 0.9, -1)
+     .when((f.col("kpi_valor_venda_real_orcado") > 0.9) & (f.col("kpi_valor_venda_real_orcado") < 1.1), 0)
+     .when(f.col("kpi_valor_venda_real_orcado") > 1.1, 1)
+)
+df_kpis_vendas_consolidado = df_kpis_vendas_consolidado.withColumn(
+    "KPI_Valor_da_Venda_Como_Estarei",
+    f.when(f.col("kpi_valor__venda_tendencia_orcado") <= 0.9, -1)
+     .when((f.col("kpi_valor__venda_tendencia_orcado") > 0.9) & (f.col("kpi_valor__venda_tendencia_orcado") < 1.1), 0)
+     .when(f.col("kpi_valor__venda_tendencia_orcado") > 1.1, 1)
+)
+
+# COMMAND ----------
+
+display(df_kpis_vendas_consolidado.orderBy('cod_mes'))
+
+# COMMAND ----------
+
+df_kpis_vendas_consolidado.write.format("delta").mode("overwrite").saveAsTable("databricks_project.dw_atacadez.kpis_vendas")
+
+# COMMAND ----------
+
+df_kpis_vendas_consolidado.select(f.mean('KPI_Valor_da_Venda_Como_Estou')).show()
